3cafd9eaf263fb8ed70e4524ad45861b82dc0d23
JckXia <ys3xia@uwaterloo.ca>
2024-11-14 11:43:32 -0500

    Add the Context Struct

--- statefun-sdk-elixir/lib/state_function.ex
+++ statefun-sdk-elixir/lib/state_function.ex
@@ -15,6 +15,11 @@ defmodule StateFun do
         {function_spec.type_name, function_spec}
     end
 
+    defmodule Message do
+        defstruct [targetAddress: nil, payload: nil]
+          
+    end
+
 
     @impl True
     def handle_call({:async_invoke, raw_body}, _from, state) do 
@@ -29,7 +34,8 @@ defmodule StateFun do
 
         stateReceivedFromFlink = StateFun.Address.AddressedScopedStorage.indexReceivedStateFromFlink(protoInvocationRequest.state)
 
-        applyBatch(protoInvocationRequest.invocations, %{},  target_function_spec.function_callback)
+        contextObject = StateFun.Context.init(funcAddress)
+        applyBatch(protoInvocationRequest.invocations, contextObject ,  target_function_spec.function_callback)
 
         # TODO aggregate results
         # collectmutation
@@ -47,19 +53,17 @@ defmodule StateFun do
 
     defp applyBatch(pbInvocationRequest, context, func_cb) do
         pbInvocationRequest 
-        |> Enum.map(fn invocation -> process_invocation_request(invocation, func_cb) end)
-    
+        |> Enum.map(fn invocation -> process_invocation_request(invocation, context, func_cb) end)
     end
 
     # 
-    defp process_invocation_request(invocation, func) do
+    defp process_invocation_request(invocation, context, func) do
         arg = invocation.argument
-        message = StateFun.Message.init(arg.typename, arg.value)
-        context = %{} # TODO logic adding context
+        message = %StateFun.Message{targetAddress: nil, payload: arg.value}
         func.(context, message)
     end
     
-    # TODO error handling if function spec
+    # TODO error handling (a bunch of different places really)
     defp get_function_spec(state, funcAddress) do 
         IO.inspect("State #{inspect(state)}, func_type: #{inspect(funcAddress.func_type)}")
         state[funcAddress.func_type]
@@ -78,17 +82,55 @@ defmodule StateFun do
     defp assert_protobuf_req_not_null(invocation_request) do
     end
 
-
     # TODO refactor these into seperate files
-    defmodule Message do
+
+ 
+
+
+    defmodule EgressMessage do 
         defstruct [:typename, :payload]
-        def init(typename, payload) do 
-            %Message{typename: typename, payload: payload}
+         def init(typename, payload) do 
+            %EgressMessage{typename: typename, payload: payload}
         end
     end
+
+    defmodule Context  do
+        alias StateFun
+        alias StateFun.Message
+        defstruct [:storage, :self, :internalContext]
+
+        defmodule InternalContext do 
+            defstruct [caller: nil, sent: [], egress: [], delayed: []]
+        end 
+     
+
+        # Self is the Address of current function
+        def init(self) do
+            %Context{storage: %{}, self: self, internalContext: %InternalContext{}}
+        end
+
+        def send(context, %StateFun.Message{} = msg) do 
+            IO.inspect("Dispatching general messages") 
+            
+            updatedSent = [msg] ++ context.internalContext.sent
+            internalContext = %InternalContext{context.internalContext | sent: updatedSent}
+    
+            %Context{context | internalContext: internalContext}
+        end
+
+        def send(context, %StateFun.EgressMessage{} = msg) do 
+            IO.inspect("Dispatch egress messages!")
+            context
+        end 
+
+        # TODO once we are confident simple send works
+        def sendAfter(delay, message, cancellationToken) do end
+        def cancelDelayedMessage(cancellationToken) do end
+    end
+ 
+  
     defmodule Address.AddressedScopedStorage do
         # Shouold be an list
-
         # TODO handle state updates
         def indexReceivedStateFromFlink(protoState) do
             IO.inspect("Received state from #{inspect(protoState)}")
